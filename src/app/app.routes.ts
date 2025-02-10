2b2158dc6e1cf7fc6ae28ce4d008768320e6234f
Fbshie <rivatyas94@gmail.com>
2025-02-10 11:44:58 +0700

    set 1

---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { MahasiswaComponent } from './mahasiswa/mahasiswa.component';
+
+
+export const routes: Routes = [
+    { path: "mahasiswa", component: MahasiswaComponent},
+];
+
